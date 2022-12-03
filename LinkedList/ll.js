3675f0146546bef95c5b1300a9d0883a0ce25e2a
koto253 <koto253@outlook.com>
2022-12-02 22:08:10 -0500

    add pop method

--- LinkedList/ll.js
+++ LinkedList/ll.js
@@ -11,6 +11,8 @@ class LinkedList {
       this.tail = newNode
       this.length = 1
    }
+
+   // add push method
    push(value) {
       let newNode = new Node(value)
       if(!this.head) {
@@ -24,12 +26,31 @@ class LinkedList {
       this.length++
       return this
    }
+
+   // add pop method 
+   pop() {
+      if(!this.head) return "it's empty" 
+      // declare 2 variable to keep track the linkedlist
+      let temp = this.head
+      let pre = this.head
+
+      while(temp.next) {
+         pre = temp
+         temp = temp.next
+      }
+      this.tail = pre
+      this.tail.next = null
+      this.length--
+      if(this.length === 0) {
+         this.head = null
+         this.tail = null
+      }
+      return temp
+   }
 }
 
 let newLinkedList = new LinkedList(4)
-// push more numbers
-newLinkedList.push(7)
-newLinkedList.push(78)
-newLinkedList.push(13)
+
+
 
 
