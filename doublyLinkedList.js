372d742b6138a9ceef45d95e32067aab3cbe026b
koto253 <koto253@outlook.com>
2022-12-04 19:29:57 -0500

    add push method

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -14,6 +14,22 @@ class DoublyLinkedList {
       this.tail = newNode
       this.length = 1
    }
+
+   // add push method 
+   push(value) {
+      let newNode = new Node(value)
+      if(!this.head) {
+         this.head = newNode
+         this.tail = newNode
+      }
+      else {
+         this.tail.next = newNode
+         newNode.prev = this.tail
+         this.tail = newNode
+      }
+      this.length++
+      return this
+   }
 }
 
 let newDoublyLinkedList = new DoublyLinkedList(7)
