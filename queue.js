3465f94a23f43a6942b76a9c306377b0d8bf813e
koto253 <koto253@outlook.com>
2022-12-06 12:50:55 -0500

    add enqueue and dequeue method

--- queue.js
+++ queue.js
@@ -13,6 +13,35 @@ class Queue {
       this.last = null
       this.length = 0
    }
+
+   // add enqueue method
+   enqueue(value) {
+      let newNode = new Node(value)
+      if(!this.first){
+         this.first = newNode
+         this.last = this.first
+      } else {
+         this.last.next = newNode
+         this.last = newNode
+      }
+      this.length++
+      return this
+   }
+
+   // add dequeue method
+   dequeue() {
+      if(!this.first) return `empty`
+      let temp = this.first
+      this.first = temp.next
+      temp.next = null
+      this.length--
+      return temp
+   }
 }
 
 let newQueue = new Queue()
+
+newQueue.enqueue(3)
+newQueue.enqueue(2)
+newQueue.enqueue(1)
+
