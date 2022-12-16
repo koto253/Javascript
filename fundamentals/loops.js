3d3967a86b3d79ba534e70630ea8c6795c6bb3ca
koto253 <koto253@outlook.com>
2022-12-15 22:45:54 -0500

    add loops

---
+++ fundamentals/loops.js
@@ -0,0 +1,18 @@
+// for loops
+function forLoop(lowNum, highNum) {
+    for(let i =lowNum; i <=highNum; i++) {
+        console.log(i)
+    }
+}
+forLoop(-1, 2);
+
+console.log(`##########################`);
+
+function whileLoop(lowNum, highNum) {
+   while(lowNum <= highNum) {
+       console.log(lowNum);
+       lowNum++;
+   }
+}
+whileLoop(-1, 2)
+
