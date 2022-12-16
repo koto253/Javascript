b6ce76c41d070c9db080c3a5dc42d60356e30a56
koto253 <koto253@outlook.com>
2022-12-15 22:37:04 -0500

    add function

--- fundamentals/functions.js
+++ fundamentals/functions.js
@@ -12,3 +12,18 @@ function goodbye(name) {
  console.log(goodbye("Ismael")); // => "Bye Ismael."
  console.log(goodbye("Aissatou")); // => "Bye Aissatou."
  console.log(goodbye("Didi")); // => "Bye Didi."
+
+ /*
+ Practice typing the syntax for functions. 
+ Write 10 functions with different messages 
+ and console.log them. Focus on using proper 
+ style and indentation. Be sure to test your 
+ code by running it after each repetition.
+ */
+
+ function sayHello(name) {
+   let msg = "Hello, " + name + ". How are you?";
+   return msg;
+ }
+ 
+ console.log(sayHello("world"));
