e2d3c69c3bdd9774e826ec16159529824352dacb
koto253 <koto253@outlook.com>
2022-12-28 21:40:34 -0500

    add pivot helper function

--- Algorithms/Sorting/quickSort.js
+++ Algorithms/Sorting/quickSort.js
@@ -3,3 +3,19 @@ function swap(arr, firstIndex, secondIndex) {
    arr[firstIndex] = arr[secondIndex];
    arr[secondIndex] = temp;
 }
+
+function pivot(arr, pivotIndex = 0, endIndex = arr.length-1){
+   let swapIndex = pivotIndex;
+   for(let i = pivotIndex + 1; i <= endIndex; i++) {
+      if(arr[i] < arr[pivotIndex]) {
+         swapIndex++
+         swap(arr, swapIndex, i);
+      }
+   }
+
+   swap(arr, pivotIndex, swapIndex);
+   return swapIndex;
+
+}
+
+console.log(pivot([4,6,1,7,3,2,5]));
