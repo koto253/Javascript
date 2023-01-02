064dd0528f18889556f650752162f930b2c42d02
koto253 <koto253@outlook.com>
2023-01-02 10:23:23 -0500

    rewrite BST

--- DataStructures/binarySearchTree.js
+++ DataStructures/binarySearchTree.js
@@ -63,18 +63,7 @@ class BinarySearchTree {
    }
 
    BFS(){
-      let currentNode = this.root;
-      let queue = [];
-      let results = [];
-      queue.push(currentNode)
-
-      while(queue.length) {
-         currentNode = queue.shift();
-         results.push(currentNode.value);
-         if(currentNode.left) queue.push(currentNode.left);
-         if(currentNode.right) queue.push(currentNode.right);
-      }
-      return results;
+      
    }
 
    // preOrder Depth First Search
