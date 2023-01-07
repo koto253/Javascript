76d1e1b99fb8c0d5ef3273fb4794d6b0d9d72838
koto253 <koto253@outlook.com>
2023-01-07 12:23:52 -0500

    2D Array - DS

---
+++ CodeChallenge/twoDArrayDS.js
@@ -0,0 +1,18 @@
+
+
+function hourglassSum(arr) {
+   // Write your code here
+   let maxSum = -63;
+   for (let i = 0; i < 4; i++) {
+       for (let j = 0; j < 4; j++) {
+           let top = arr[i][j] + arr[i][j+1] + arr[i][j+2];
+           let mid = arr[i+1][j+1];
+           let bottom = arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+           
+           let sum = top + mid + bottom;
+           maxSum = Math.max(maxSum, sum); 
+       }
+   }
+   // The function should return an INTEGER!! 
+   return maxSum;
+}
